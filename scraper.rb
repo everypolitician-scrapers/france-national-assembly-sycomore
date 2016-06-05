4b3ef0df57d63ef38d19d4d5600f0f69bfd17d4d
Tony Bowden <tony@tmtm.com>
2016-06-05 09:37:39 +0100

    Fewer terms (for now)

--- scraper.rb
+++ scraper.rb
@@ -103,4 +103,5 @@ def scrape_person(url, data)
   end
 end
 
-(46..59).to_a.reverse_each { |termid| scrape_term(termid) }
+# (46..59).to_a.reverse_each { |termid| scrape_term(termid) }
+(58..59).to_a.reverse_each { |termid| scrape_term(termid) }
