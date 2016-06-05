33c25162e779e494a811aec4d693193cf6621451
Tony Bowden <tony@tmtm.com>
2016-06-05 15:06:19 +0100

    More historic terms

--- scraper.rb
+++ scraper.rb
@@ -135,5 +135,4 @@ def scrape_person(url, data)
   end
 end
 
-# (46..59).to_a.reverse_each { |termid| scrape_term(termid) }
-(58..59).to_a.reverse_each { |termid| scrape_term(termid) }
+(46..59).to_a.reverse_each { |termid| scrape_term(termid) }
